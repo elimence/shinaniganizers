9261804145f4549cfdf68c7d6d964d990fe1a8b6
nanaewusi <precious.ewusi.nyarko@meltwater.org>
2014-01-16 04:48:38 +0000

    Added Field Officer & BirthRecord Schemas

--- model/db.js
+++ model/db.js
@@ -18,7 +18,44 @@ mongoose.connect(dbUriString, function (err, conn) {
 });
 
 // Schema & Model Definitions
+// Field Officers
+var FieldOfficerSchema = new Schema({
+  firstname: String,
+  lastname: String,
+  region: String,
+  district: String,
+  municipality: String,
+  community: String,
+  phone: String,
+  username: String,
+  password: String,
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
+});
 
+// Birth Records
+var BirthRecordSchema = new Schema({
+  fullname: String,
+  placeofbirth: String,
+  dateofbirth: String,
+  gender: String,
+  mothersmaidenname: String,
+  sender: {
+    type: Schema.ObjectId,
+    ref: 'FieldOfficer'
+  },
+  hasCertificate: {
+    type: Boolean,
+    default: false
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
+});
 
 // Compile Schemas into Models
-// mongoose.model( 'Director', DirectorSchema );
+mongoose.model( 'FieldOfficer', FieldOfficerSchema );
+mongoose.model( 'BirthRecord', BirthRecordSchema );
