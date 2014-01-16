f831b53723ed2c8a2eb1540b1d5d38beee661ed1
nanaewusi <precious.ewusi.nyarko@meltwater.org>
2014-01-16 15:13:29 +0000

    More more changes to reflect new app. Can now log analyst in from the login page. Route tracks sessions

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,59 @@
 // Module Dependencies
-var mongoose = require('mongoose');
+var Pass = require('../helpers/password'),
+    Analyst = require('../model/analyst'),
+    step = require('async');
 
 var init = function(app) {
-	// Index Page
+	// Login Page
 	app.get('/', function (req, res) {
     if (req.session.user) {
       res.redirect('/dashboard');
     } else {
       var badLogin = (req.query.status == 'badLogin') ? true : false;
       res.render('index', {
-        title: "BirthsReport - An Automated Births Registry for Ghana's hard to reach.",
+        title: "SafeWater - Improving information flow for safe water in Ghana.",
         hasErrors: badLogin
       });
     }
 	});
+
+  // Login Attempt
+  app.post('/', function (req, res) {
+    step.waterfall([
+        function(callback){
+          Analyst.findByUsername(req.body.username, function (err, user) {
+            if (err) callback(err);
+            else if (!user) callback(null, false); // Second Argument is for the validity of the login
+            else {
+              var isValidLogin = Pass.validate(user.password, req.body.password);
+              if (isValidLogin) callback(null, true, user);
+              else callback(null, false, null);
+            }
+          });
+        }
+    ], function (err, validLogin, _user) {
+      if (err || (!validLogin)) {
+        res.redirect('/' + '?status=badLogin');
+      }
+      else {
+        req.session.user = _user;
+        res.redirect('/');
+      }
+    });
+  });
+
+  // New Analyst ##Remove Later
+  // app.get('/analyst/new', function (req, res) {
+  //   Analyst.save({
+  //     firstname: 'Samuel',
+  //     lastname: 'Ako',
+  //     username: 'kozzle',
+  //     password: 'bSvMUfurp49acd97afa91f652fa7a733f1974abde'
+  //   }, function (err, saved) {
+  //     if (err) res.send('Something went wrong');
+  //     else res.send(saved);
+  //   })
+  // });
 }
 
 exports.init = init;
